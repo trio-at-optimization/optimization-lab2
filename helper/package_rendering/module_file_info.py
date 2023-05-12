329f078070e30d0461b2c9feeeac7bebd1a82239
Nomad192 <verenya08@gmail.com>
2023-05-13 00:07:51 +0300

    T1 multi preview

--- helper/package_rendering/module_file_info.py
+++ helper/package_rendering/module_file_info.py
@@ -25,11 +25,11 @@ def random_generator(real_weights, real_bias, dots_count=100, variance=1):
 
 
 class file_info_2d_data:
-    def __init__(self, real_weights, real_bias, full_loss_f=[], dots_count=100, variance=1, labels_loss=None,
+    def __init__(self, real_weight, real_bias, full_loss_f=[], dots_count=100, variance=1, labels_loss=[],
                  current_random_generator=random_generator):
-        self.real_weights = real_weights
+        self.real_weight = real_weight
         self.real_bias = real_bias
-        self.X, self.y = current_random_generator(real_weights, real_bias, dots_count, variance)
+        self.X, self.y = current_random_generator([real_weight], real_bias, dots_count, variance)
         self.loss_f = []
         for i in range(len(full_loss_f)):
             self.loss_f.append(lambda value: full_loss_f[i](self.X, self.y, value))
