3c683d2b11641e6276565d2bb0ff305602bdb04b
Nomad192 <verenya08@gmail.com>
2023-05-14 12:23:45 +0300

    T1 real Minibatch draft

--- helper/package_T1/__init__.py
+++ helper/package_T1/__init__.py
@@ -1,2 +1,2 @@
 from .module_dataset import *
-from .module_stochastic_gradient_descent import *
+from .module_minibatch_gradient_descent import *
--- helper/package_T1/module_minibatch_gradient_descent.py
+++ helper/package_T1/module_minibatch_gradient_descent.py
@@ -1,15 +1,20 @@
-from .module_gradient import gradient
-
-# -------------------------------------------------------------------------------------------------------------------- #
-# Для расчетов 
+# Для расчетов
 import numpy as np
 
 
 # ==================================================================================================================== #
 # Градиентный спуск
 
-def custom_gradient_descent_with_lr_search(f, custom_gradient_function, x0, lr_search_func, num_iterations=1000,
-                                           eps=1e-6, step_size=1, minimum=0.0, apply_min=False):
+def custom_gradient_descent_with_lr_scheduling(
+                                               f
+                                               , gradient, x0
+                                               , lr_scheduling_func
+                                               , initial_lr=1
+                                               , num_iterations=1000
+                                               , eps=1e-6
+                                               , minimum=0.0
+                                               , apply_min=False
+                                               ):
     """
     Функция вычисления градиентного спуска с заданной функцией поиска коэффициента обучения
 
@@ -20,7 +25,7 @@ def custom_gradient_descent_with_lr_search(f, custom_gradient_function, x0, lr_s
     lr_search_func -- функция поиска оптимального коэффициента обучения (learning rate)
         Аргументы:
         f -- функция
-        custom_gradient_function -- функция градиента
+        gradient -- функция градиента
         a -- левая граница интервала
         b -- правая граница интервала
         eps -- точность поиска
@@ -36,26 +41,15 @@ def custom_gradient_descent_with_lr_search(f, custom_gradient_function, x0, lr_s
     points -- массив оптимальных на каждом шаге точек
     """
 
-    def line_search(x_dot, d):
-        def fd(alpha):
-            return f(x_dot - alpha * d)
-
-        return lr_search_func(fd, 0, 1, eps)
-
     x = np.copy(x0)
-    points = np.array([x])
+    points = [x.copy()]
     for i in range(num_iterations):
         if apply_min and abs(f(x) - minimum) < eps:
             break
 
-        grad_x = custom_gradient_function(f, x)
-        x = x - grad_x * line_search(x, grad_x) * step_size
-        points = np.vstack([points, x])
-    return points
-
-
-def gradient_descent(f, x0, lr_search_func, num_iterations=1000, eps=1e-6, step_size=1, minimum=0.0, apply_min=False):
-    return custom_gradient_descent_with_lr_search(f, gradient, x0, lr_search_func, num_iterations, eps, step_size,
-                                                  minimum, apply_min)
+        grad_x = gradient(f, x)
+        x = x - grad_x * lr_scheduling_func(i, initial_lr)
+        points.append(x.copy())
+    return np.array(points)
 
 # ==================================================================================================================== #
